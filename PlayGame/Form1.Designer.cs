521ad58fac555078d9dbb694d92d391c8ecaa3ed
GarrSaun <garrsaun253@ed.amdsb.ca>
2017-09-18 13:01:55 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlayGame
+{
+    partial class background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(background));
+            this.playButton = new System.Windows.Forms.Button();
+            this.start3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(120, 131);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(285, 122);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // start3
+            // 
+            this.start3.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start3.ForeColor = System.Drawing.Color.Black;
+            this.start3.Location = new System.Drawing.Point(163, 299);
+            this.start3.Name = "start3";
+            this.start3.Size = new System.Drawing.Size(203, 32);
+            this.start3.TabIndex = 1;
+            this.start3.Text = "Game Starts In: 3";
+            this.start3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(512, 385);
+            this.Controls.Add(this.start3);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "background";
+            this.Text = "playGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label start3;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class background : Form
+    {
+        public background()
+        {
+            InitializeComponent();
+        }
+
+        SoundPlayer ding = new SoundPlayer(Properties.Resources.up);
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playButton.Visible = false;
+            start3.ForeColor = Color.White;
+            ding.Play();
+            this.Refresh();
+            Thread.Sleep(1000);
+
+            start3.Text = "Game Starts In: 2";
+            ding.Play();
+            this.Refresh();
+            Thread.Sleep(1000);
+
+            start3.Text = "Game Starts In: 1";
+            ding.Play();
+            this.Refresh();
+            Thread.Sleep(1000);
+
+            //Graphics Stuff
+            start3.Visible = false;
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.LimeGreen);
+            Pen drawPen = new Pen(Color.Red, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+            //Red Ellipse
+            formGraphics.DrawEllipse(drawPen, 159, 87, 200, 200);
+            formGraphics.FillEllipse(drawBrush, 159, 87, 200, 200);
+
+            //C
+            drawBrush.Color = Color.White;
+            Font cFont = new Font("Copperplate Gothic", 120, FontStyle.Bold);
+            formGraphics.DrawString("C", cFont, drawBrush, 150, 100);
+
+            //Rams
+            Font ramFont = new Font("Segoe UI", 25, FontStyle.Bold);
+            formGraphics.DrawString("Rams", ramFont, drawBrush, 240, 163);
+
+            //Rotated Text
+            Font turnFont = new Font("Sugoe UI", 10, FontStyle.Bold);
+            formGraphics.TranslateTransform(375, 260);
+            formGraphics.RotateTransform(270);
+            formGraphics.DrawString("Central Rams Arcade", turnFont, drawBrush, new Rectangle());
+            formGraphics.ResetTransform();
+        }
+    }
+}
